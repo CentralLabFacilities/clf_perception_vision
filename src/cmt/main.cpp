f1cfaf51970812b0fc7341669909981051140357
fl <fl@techfak.uni-bielefeld.de>
2017-07-18 16:51:55 +0200

    cmt working for now

--- src/cmt/main.cpp
+++ src/cmt/main.cpp
@@ -1,9 +1,15 @@
+// SELF
 #include "CMT.h"
 #include "gui.h"
+#include "ros_grabber.hpp"
 
+// CV
+#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/utility.hpp>
 
+// STD
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -33,30 +39,10 @@ using std::endl;
 using ::atof;
 
 static string WIN_NAME = "CMT";
-static string OUT_FILE_COL_HEADERS =
-    "Frame,Timestamp (ms),Active points,"\
-    "Bounding box centre X (px),Bounding box centre Y (px),"\
-    "Bounding box width (px),Bounding box height (px),"\
-    "Bounding box rotation (degrees),"\
-    "Bounding box vertex 1 X (px),Bounding box vertex 1 Y (px),"\
-    "Bounding box vertex 2 X (px),Bounding box vertex 2 Y (px),"\
-    "Bounding box vertex 3 X (px),Bounding box vertex 3 Y (px),"\
-    "Bounding box vertex 4 X (px),Bounding box vertex 4 Y (px)";
 
-vector<float> getNextLineAndSplitIntoFloats(istream& str)
-{
-    vector<float>   result;
-    string                line;
-    getline(str,line);
+string topic = "/usb_cam/image_raw";
+bool pyr = false;
 
-    stringstream          lineStream(line);
-    string                cell;
-    while(getline(lineStream,cell,','))
-    {
-        result.push_back(atof(cell.c_str()));
-    }
-    return result;
-}
 
 int display(Mat im, CMT & cmt)
 {
@@ -79,43 +65,17 @@ int display(Mat im, CMT & cmt)
     return waitKey(5);
 }
 
-string write_rotated_rect(RotatedRect rect)
-{
-    Point2f verts[4];
-    rect.points(verts);
-    stringstream coords;
-
-    coords << rect.center.x << "," << rect.center.y << ",";
-    coords << rect.size.width << "," << rect.size.height << ",";
-    coords << rect.angle << ",";
-
-    for (int i = 0; i < 4; i++)
-    {
-        coords << verts[i].x << "," << verts[i].y;
-        if (i != 3) coords << ",";
-    }
-
-    return coords.str();
-}
-
 int main(int argc, char **argv)
 {
+    // ROS
+    ros::init(argc, argv, "clf_cmt", ros::init_options::AnonymousName);
+
     //Create a CMT object
     CMT cmt;
-
-    //Initialization bounding box
-    Rect rect;
 
     //Parse args
     int challenge_flag = 0;
     int loop_flag = 0;
-    int verbose_flag = 0;
-    int bbox_flag = 0;
-    int skip_frames = 0;
-    int skip_msecs = 0;
-    int output_flag = 0;
-    string input_path;
-    string output_path;
 
     const int detector_cmd = 1000;
     const int descriptor_cmd = 1001;
@@ -125,334 +85,74 @@ int main(int argc, char **argv)
     const int skip_cmd = 1005;
     const int skip_msecs_cmd = 1006;
     const int output_file_cmd = 1007;
-
-    struct option longopts[] =
-    {
-        //No-argument options
-        {"challenge", no_argument, &challenge_flag, 1},
-        {"loop", no_argument, &loop_flag, 1},
-        {"verbose", no_argument, &verbose_flag, 1},
-        {"no-scale", no_argument, 0, no_scale_cmd},
-        {"with-rotation", no_argument, 0, with_rotation_cmd},
-        //Argument options
-        {"bbox", required_argument, 0, bbox_cmd},
-        {"detector", required_argument, 0, detector_cmd},
-        {"descriptor", required_argument, 0, descriptor_cmd},
-        {"output-file", required_argument, 0, output_file_cmd},
-        {"skip", required_argument, 0, skip_cmd},
-        {"skip-msecs", required_argument, 0, skip_msecs_cmd},
-        {0, 0, 0, 0}
-    };
-
-    int index = 0;
-    int c;
-    while((c = getopt_long(argc, argv, "v", longopts, &index)) != -1)
-    {
-        switch (c)
-        {
-            case 'v':
-                verbose_flag = true;
-                break;
-            case bbox_cmd:
-                {
-                    //TODO: The following also accepts strings of the form %f,%f,%f,%fxyz...
-                    string bbox_format = "%f,%f,%f,%f";
-                    float x,y,w,h;
-                    int ret = sscanf(optarg, bbox_format.c_str(), &x, &y, &w, &h);
-                    if (ret != 4)
-                    {
-                        cerr << "bounding box must be given in format " << bbox_format << endl;
-                        return 1;
-                    }
-
-                    bbox_flag = 1;
-                    rect = Rect(x,y,w,h);
-                }
-                break;
-            case detector_cmd:
-                cmt.str_detector = optarg;
-                break;
-            case descriptor_cmd:
-                cmt.str_descriptor = optarg;
-                break;
-            case output_file_cmd:
-                output_path = optarg;
-                output_flag = 1;
-                break;
-            case skip_cmd:
-                {
-                    int ret = sscanf(optarg, "%d", &skip_frames);
-                    if (ret != 1)
-                    {
-                      skip_frames = 0;
-                    }
-                }
-                break;
-            case skip_msecs_cmd:
-                {
-                    int ret = sscanf(optarg, "%d", &skip_msecs);
-                    if (ret != 1)
-                    {
-                      skip_msecs = 0;
-                    }
-                }
-                break;
-            case no_scale_cmd:
-                cmt.consensus.estimate_scale = false;
-                break;
-            case with_rotation_cmd:
-                cmt.consensus.estimate_rotation = true;
-                break;
-            case '?':
-                return 1;
-        }
-
-    }
-
-    // Can only skip frames or milliseconds, not both.
-    if (skip_frames > 0 && skip_msecs > 0)
-    {
-      cerr << "You can only skip frames, or milliseconds, not both." << endl;
-      return 1;
-    }
-
-    //One argument remains
-    if (optind == argc - 1)
-    {
-        input_path = argv[optind];
-    }
-
-    else if (optind < argc - 1)
-    {
-        cerr << "Only one argument is allowed." << endl;
-        return 1;
-    }
-
-    //Set up logging
-    FILELog::ReportingLevel() = verbose_flag ? logDEBUG : logINFO;
-    Output2FILE::Stream() = stdout; //Log to stdout
-
-    //Challenge mode
-    if (challenge_flag)
-    {
-        //Read list of images
-        ifstream im_file("images.txt");
-        vector<string> files;
-        string line;
-        while(getline(im_file, line ))
-        {
-            files.push_back(line);
-        }
-
-        //Read region
-        ifstream region_file("region.txt");
-        vector<float> coords = getNextLineAndSplitIntoFloats(region_file);
-
-        if (coords.size() == 4) {
-            rect = Rect(coords[0], coords[1], coords[2], coords[3]);
-        }
-
-        else if (coords.size() == 8)
-        {
-            //Split into x and y coordinates
-            vector<float> xcoords;
-            vector<float> ycoords;
-
-            for (size_t i = 0; i < coords.size(); i++)
-            {
-                if (i % 2 == 0) xcoords.push_back(coords[i]);
-                else ycoords.push_back(coords[i]);
-            }
-
-            float xmin = *min_element(xcoords.begin(), xcoords.end());
-            float xmax = *max_element(xcoords.begin(), xcoords.end());
-            float ymin = *min_element(ycoords.begin(), ycoords.end());
-            float ymax = *max_element(ycoords.begin(), ycoords.end());
-
-            rect = Rect(xmin, ymin, xmax-xmin, ymax-ymin);
-            cout << "Found bounding box" << xmin << " " << ymin << " " <<  xmax-xmin << " " << ymax-ymin << endl;
-        }
-
-        else {
-            cerr << "Invalid Bounding box format" << endl;
-            return 0;
-        }
-
-        //Read first image
-        Mat im0 = imread(files[0]);
-        Mat im0_gray;
-        cvtColor(im0, im0_gray, CV_BGR2GRAY);
-
-        //Initialize cmt
-        cmt.initialize(im0_gray, rect);
-
-        //Write init region to output file
-        ofstream output_file("output.txt");
-        output_file << rect.x << ',' << rect.y << ',' << rect.width << ',' << rect.height << std::endl;
-
-        //Process images, write output to file
-        for (size_t i = 1; i < files.size(); i++)
-        {
-            FILE_LOG(logINFO) << "Processing frame " << i << "/" << files.size();
-            Mat im = imread(files[i]);
-            Mat im_gray;
-            cvtColor(im, im_gray, CV_BGR2GRAY);
-            cmt.processFrame(im_gray);
-            if (verbose_flag)
-            {
-                display(im, cmt);
-            }
-            rect = cmt.bb_rot.boundingRect();
-            output_file << rect.x << ',' << rect.y << ',' << rect.width << ',' << rect.height << std::endl;
-        }
-
-        output_file.close();
-
-        return 0;
-    }
-
-    //Normal mode
+    unsigned int last_computed_frame = -1;
 
     //Create window
     namedWindow(WIN_NAME);
 
-    VideoCapture cap;
-
     bool show_preview = true;
 
-    //If no input was specified
-    if (input_path.length() == 0)
-    {
-        cap.open(0); //Open default camera device
+    cv::CommandLineParser parser(argc, argv, "{@config |<none>| yaml config file}" "{help h ||}");
+    cv::FileStorage fs(argv[1], cv::FileStorage::READ);
+
+    if (fs.isOpened()) {
+
+        fs["input_ros_topic"] >> topic;
+        cout << ">>> Input Topic: --> " << topic << endl;
+
+        fs["pyr"] >> pyr;
+        cout << ">>> PyrUP: --> " << pyr << endl;
     }
 
-    //Else open the video specified by input_path
-    else
-    {
-        cap.open(input_path);
+    fs.release();
 
-        if (skip_frames > 0)
-        {
-          cap.set(CV_CAP_PROP_POS_FRAMES, skip_frames);
-        }
-
-        if (skip_msecs > 0)
-        {
-          cap.set(CV_CAP_PROP_POS_MSEC, skip_msecs);
-
-          // Now which frame are we on?
-          skip_frames = (int) cap.get(CV_CAP_PROP_POS_FRAMES);
-        }
-
-        show_preview = false;
-    }
-
-    //If it doesn't work, stop
-    if(!cap.isOpened())
-    {
-        cerr << "Unable to open video capture." << endl;
-        return -1;
-    }
-
-    //Show preview until key is pressed
-    while (show_preview)
-    {
-        Mat preview;
-        cap >> preview;
-
-        screenLog(preview, "Press a key to start selecting an object.");
-        imshow(WIN_NAME, preview);
-
-        char k = waitKey(10);
-        if (k != -1) {
-            show_preview = false;
-        }
-    }
-
-    //Get initial image
-    Mat im0;
-    cap >> im0;
-
-    //If no bounding was specified, get it from user
-    if (!bbox_flag)
-    {
-        rect = getRect(im0, WIN_NAME);
-    }
-
-    FILE_LOG(logINFO) << "Using " << rect.x << "," << rect.y << "," << rect.width << "," << rect.height
-        << " as initial bounding box.";
-
-    //Convert im0 to grayscale
-    Mat im0_gray;
-    if (im0.channels() > 1) {
-        cvtColor(im0, im0_gray, CV_BGR2GRAY);
-    } else {
-        im0_gray = im0;
-    }
+    // ROS
+    ROSGrabber ros_grabber(topic);
+    ros_grabber.setPyr(pyr);
 
     //Initialize CMT
+    cv::Rect rect;
+    rect.x = 28;
+    rect.y = 27;
+    rect.width = 100;
+    rect.height = 100;
+
+    Mat im0, im0_gray;
+
+    while (im0.empty()) {
+        ros::spinOnce();
+        ros_grabber.getImage(&im0);
+    }
+
+    cvtColor(im0, im0_gray, CV_BGR2GRAY);
     cmt.initialize(im0_gray, rect);
 
-    int frame = skip_frames;
-
-    //Open output file.
-    ofstream output_file;
-
-    if (output_flag)
-    {
-        int msecs = (int) cap.get(CV_CAP_PROP_POS_MSEC);
-
-        output_file.open(output_path.c_str());
-        output_file << OUT_FILE_COL_HEADERS << endl;
-        output_file << frame << "," << msecs << ",";
-        output_file << cmt.points_active.size() << ",";
-        output_file << write_rotated_rect(cmt.bb_rot) << endl;
-    }
+    //The image
+    Mat im;
 
     //Main loop
     while (true)
     {
-        frame++;
-
-        Mat im;
-
-        //If loop flag is set, reuse initial image (for debugging purposes)
-        if (loop_flag) im0.copyTo(im);
-        else cap >> im; //Else use next image in stream
-
-        if (im.empty()) break; //Exit at end of video stream
-
-        Mat im_gray;
-        if (im.channels() > 1) {
-            cvtColor(im, im_gray, CV_BGR2GRAY);
-        } else {
-            im_gray = im;
+        ros::spinOnce();
+        ros_grabber.getImage(&im);
+        int tmp_frame_nr = ros_grabber.getLastFrameNr();
+        if(last_computed_frame != tmp_frame_nr) {
+            if (im.empty()) continue; //Exit at end of video stream
+            Mat im_gray;
+            if (im.channels() > 1) {
+                cvtColor(im, im_gray, CV_BGR2GRAY);
+            } else {
+                im_gray = im;
+            }
+            // Let CMT process the frame
+            cmt.processFrame(im_gray);
+            // cmt.points_active.size()
         }
-
-        //Let CMT process the frame
-        cmt.processFrame(im_gray);
-
-        //Output.
-        if (output_flag)
-        {
-            int msecs = (int) cap.get(CV_CAP_PROP_POS_MSEC);
-            output_file << frame << "," << msecs << ",";
-            output_file << cmt.points_active.size() << ",";
-            output_file << write_rotated_rect(cmt.bb_rot) << endl;
-        }
-        else
-        {
-            //TODO: Provide meaningful output
-            FILE_LOG(logINFO) << "#" << frame << " active: " << cmt.points_active.size();
-        }
-
+        last_computed_frame = ros_grabber.getLastFrameNr();
         //Display image and then quit if requested.
         char key = display(im, cmt);
         if(key == 'q') break;
     }
-
-    //Close output file.
-    if (output_flag) output_file.close();
 
     return 0;
 }
