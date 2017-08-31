7c539b13abf7577569e1d52112d520cb6479e4c6
Florian Lier <fl@techfak.uni-bielefeld.de>
2017-08-31 20:29:31 +0200

    debugging input topic

--- src/clf_perception_surb.cpp
+++ src/clf_perception_surb.cpp
@@ -136,7 +136,7 @@ int Detect2D::setup(int argc, char *argv[]) {
     if (fs.isOpened()) {
 
         fs["ros_input_topic"] >> ros_input_topic;
-        cout << ">>> ROS input topic --> " << type_descriptor  << endl;
+        cout << ">>> ROS input topic --> " << ros_input_topic  << endl;
 
         fs["keypointalgo"] >> type_descriptor;
         cout << ">>> Keypoint Descriptor --> " << type_descriptor  << endl;
