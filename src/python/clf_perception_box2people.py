7595f055420a435556f1ce7135964e00039f7d51
Florian Lier <fl@techfak.uni-bielefeld.de>
2017-09-01 18:02:54 +0200

    Added probability and cut off

--- src/python/clf_perception_box2people.py
+++ src/python/clf_perception_box2people.py
@@ -2,43 +2,49 @@
 
 import rospy
 import std_msgs.msg
+from optparse import OptionParser
 from darknet_ros_msgs.msg import BoundingBoxes, BoundingBox
 from clf_perception_vision.msg import ExtenedPeople, ExtendedPersonStamped
 
 
-def box_cb(data):
-    try:
-        e = ExtenedPeople()
-        h = std_msgs.msg.Header()
-        h.stamp = rospy.Time.now()
-        for person in data.boundingBoxes:
-            if person.Class == "person":
-                p = ExtendedPersonStamped()
-                p.header = h
-                p.bbox_xmin = person.xmin
-                p.bbox_xmax = person.xmax
-                p.bbox_ymin = person.ymin
-                p.bbox_ymax = person.ymax
-                p.probability = person.probability
-                e.persons.append(p)
+class BBox2ExtendedPeople:
 
-    except Exception, e:
-        rospy.logwarn(">>> problem receiving data, %s" % str(e))
+    def __init__(self, _in, _out):
+        rospy.init_node('clf_perception_vision_box2people', anonymous=True)
+        self.sub = rospy.Subscriber(str(_in), BoundingBoxes, self.box_cb, queue_size=1)
+        self.pub = rospy.Publisher(str(_out), ExtenedPeople, queue_size=1)
 
-
-rospy.init_node('clf_perception_vision_box2people', anonymous=True)
-rate = rospy.Rate(2.0)
-sub = rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, box_cb)
-pub = rospy.Publisher('/clf_perception_vision_box2people/people_raw', ExtenedPeople, queue_size=1)
-
-
-def publish_extended_people(data):
-    pass
+    def box_cb(self, data):
+        try:
+            e = ExtenedPeople()
+            h = std_msgs.msg.Header()
+            h.stamp = rospy.Time.now()
+            for person in data.boundingBoxes:
+                if person.Class == "chair":
+                    p = ExtendedPersonStamped()
+                    p.header = h
+                    p.bbox_xmin = person.xmin
+                    p.bbox_xmax = person.xmax
+                    p.bbox_ymin = person.ymin
+                    p.bbox_ymax = person.ymax
+                    p.probability = float('%.2f' % person.probability)
+                    e.persons.append(p)
+            self.pub.publish(e)
+        except Exception, ex:
+            rospy.logwarn(">>> Problem receiving data, %s" % str(ex))
 
 
-if __name__ == '__main__':
-    try:
-        while not rospy.is_shutdown():
+if __name__ == "__main__":
+
+    parser = OptionParser()
+    parser.add_option("--intopic", dest="intopic", default="/darknet_ros/bounding_boxes")
+    parser.add_option("--outtopic", dest="outtopic", default="/clf_perception_vision/people_raw")
+    (options, args) = parser.parse_args()
+    B2P = BBox2ExtendedPeople(options.intopic, options.outtopic)
+    rate = rospy.Rate(2.0)
+    while not rospy.is_shutdown():
+        try:
             rate.sleep()
-    except rospy.ROSInterruptException:
-        pass
+        except rospy.ROSInterruptException, ex:
+            rospy.logwarn(">>> Exiting, %s" % str(ex))
+
