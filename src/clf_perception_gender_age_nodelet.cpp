f4f4246ec9ce276ee5f33ef15cd65314829ac0d0
Florian Lier <fl@techfak.uni-bielefeld.de>
2017-09-03 11:31:48 +0200

    Added first launch file and parameter handling

--- src/clf_perception_gender_age_nodelet.cpp
+++ src/clf_perception_gender_age_nodelet.cpp
@@ -1,38 +1,21 @@
+// STD
+#include <stdlib.h>
+
 // ROS NODELETS
 #include <ros/ros.h>
-#include <pluginlib/class_list_macros.h>
 #include <nodelet/nodelet.h>
-#include <clf_perception_vision/ExtendedPersonStamped.h>
+#include <pluginlib/class_list_macros.h>
+
+// MSGS
 #include <clf_perception_vision/ExtenedPeople.h>
+#include <clf_perception_vision/ExtendedPersonStamped.h>
 
 // SELF
 #include "ros_grabber.hpp"
 #include "dlib_detection.hpp"
 
-
 using namespace std;
 using namespace cv;
-
-string cfg_yaml,
-       topic,
-       shape_mode_path,
-       model_file_age,
-       model_file_gender,
-       trained_file_age,
-       trained_file_gender,
-       mean_file,
-       label_file_age,
-       label_file_gender;
-
-unsigned int _pyr = 0;
-unsigned int gender_age = 1;
-unsigned int frame_count = 0;
-unsigned int average_frames = 0;
-unsigned int microseconds = 1000;
-const int fontFace = cv::FONT_HERSHEY_PLAIN;
-
-double time_spend = 0;
-const double fontScale = 1;
 
 namespace clf_perception_vision {
 
@@ -44,8 +27,32 @@ namespace clf_perception_vision {
         ros::NodeHandle private_nh;
         ros::Subscriber pose_sub;
         ros::Publisher people_pub;
+
         ROSGrabber *ros_grabber;
         DlibFace dlf;
+
+        string cfg_yaml,
+               topic,
+               out_topic,
+               people_topic,
+               shape_mode_path,
+               model_file_age,
+               model_file_gender,
+               trained_file_age,
+               trained_file_gender,
+               mean_file,
+               label_file_age,
+               label_file_gender;
+
+        unsigned int _pyr = 0;
+        unsigned int gender_age = 1;
+        unsigned int frame_count = 0;
+        unsigned int average_frames = 0;
+        unsigned int microseconds = 1000;
+        const int fontFace = cv::FONT_HERSHEY_PLAIN;
+
+        double time_spend = 0;
+        const double fontScale = 1;
 
         virtual void onInit() {
             // Get the NodeHandle
@@ -54,13 +61,42 @@ namespace clf_perception_vision {
             cout << ">>> Found --> " << cv::getNumberOfCPUs() << " CPUs"<< endl;
             // Are we using optimized OpenCV Code?
             cout << ">>> OpenCV was built with optimizations --> " << cv::useOptimized() << endl;
-            private_nh.getParam("cfg_yaml", cfg_yaml);
+
+            if (private_nh.getParam("gender_age_config", cfg_yaml))
+            {
+                ROS_INFO(">>> Config File: --> %s", cfg_yaml.c_str());
+            } else {
+                ROS_ERROR(">>> Failed to get depth topic parameter");
+                exit(EXIT_FAILURE);
+            }
+
+            if (private_nh.getParam("gender_age_in_topic", people_topic))
+            {
+                ROS_INFO(">>> Input People Topic: --> %s", people_topic.c_str());
+            } else {
+                ROS_ERROR(">>> Failed to get people topic parameter");
+                exit(EXIT_FAILURE);
+            }
+
+            if (private_nh.getParam("gender_age_out_topic", out_topic))
+            {
+                ROS_INFO(">>> Output People Topic: --> %s", out_topic.c_str());
+            } else {
+                ROS_ERROR(">>> Failed to get out topic parameter");
+                exit(EXIT_FAILURE);
+            }
+
+            if (private_nh.getParam("gender_age_image_topic", topic))
+            {
+                ROS_INFO(">>> Input Image Topic: --> %s", topic.c_str());
+            } else {
+                ROS_ERROR(">>> Failed to get input image topic parameter");
+                exit(EXIT_FAILURE);
+            }
+
             FileStorage fs(cfg_yaml, FileStorage::READ);
 
             if (fs.isOpened()) {
-                fs["input_ros_topic"] >> topic;
-                cout << ">>> Input Topic: --> " << topic << endl;
-
                 fs["dlib_shapepredictor"] >> shape_mode_path;
                 cout << ">>> Frontal Face: --> " << shape_mode_path << endl;
 
@@ -91,17 +127,22 @@ namespace clf_perception_vision {
 
                 fs["label_file_age"] >> label_file_age;
                 cout << ">>> Labels Age: --> " << label_file_age << endl;
-
             }
+
             fs.release();
-            cout << ">>> ROS In Topic --> " << topic << endl;
-            pose_sub = private_nh.subscribe("/clf_perception_vision/people/raw/pose", 10, &GenderAge::person_callback, this);
-            people_pub = private_nh.advertise<clf_perception_vision::ExtenedPeople>("/clf_perception_vision/people/raw/pose/genderage", 10);
+
+            cout << ">>> ROS In Topic: --> " << topic << endl;
+
+            pose_sub = private_nh.subscribe(people_topic.c_str(), 1, &GenderAge::person_callback, this);
+            people_pub = private_nh.advertise<clf_perception_vision::ExtenedPeople>(out_topic.c_str(), 1);
+
             ros_grabber = new ROSGrabber(topic);
             ros_grabber->setPyr(_pyr);
+
             dlf.setup(shape_mode_path);
             dlf.cl = new Classifier(model_file_gender, trained_file_gender, mean_file, label_file_gender);
             dlf.cl_age = new Classifier(model_file_age, trained_file_age, mean_file, label_file_age);
+
             cout << ">>> Ready. Let's go..." << endl;
         }
 
--- src/clf_perception_gender_depth_lookup_nodelet.cpp
+++ src/clf_perception_gender_depth_lookup_nodelet.cpp
@@ -1,14 +1,17 @@
+// STD
+#include <stdlib.h>
+
 // ROS NODELETS
 #include <ros/ros.h>
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.h>
 
 // MSGS
-#include <clf_perception_vision/ExtenedPeople.h>
-#include <clf_perception_vision/ExtendedPersonStamped.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/image_encodings.h>
+#include <clf_perception_vision/ExtenedPeople.h>
+#include <clf_perception_vision/ExtendedPersonStamped.h>
 
 // FILTER
 #include <message_filters/subscriber.h>
@@ -31,26 +34,52 @@ namespace clf_perception_vision {
 
     private:
         ros::NodeHandle private_nh;
+        ros::Publisher people_pub;
+
         string depth_topic;
         string depth_info;
+        string out_topic;
+        string in_topic;
 
         virtual void onInit() {
             // Get the NodeHandle
             private_nh = getPrivateNodeHandle();
-            if (private_nh.getParam("depth_topic", depth_topic))
+
+            if (private_nh.getParam("depthlookup_image_topic", depth_topic))
             {
-                ROS_INFO(">>> Input depth topic: %s", depth_topic.c_str());
+                ROS_INFO(">>> Input depth image topic: %s", depth_topic.c_str());
             } else {
-                ROS_ERROR("Failed to get depth topic");
+                ROS_ERROR("Failed to get depth image topic parameter");
+                exit(EXIT_FAILURE);
             }
-            if (private_nh.getParam("depth_info", depth_info))
+            if (private_nh.getParam("depthlookup_info_topic", depth_info))
             {
-                ROS_INFO(">>> Input depth info topic: %s", depth_info.c_str());
+                ROS_INFO(">>> Input depth camera info topic: %s", depth_info.c_str());
             } else {
-                ROS_ERROR("Failed to get depth info topic");
+                ROS_ERROR("Failed to get depth camera info topic parameter");
+                exit(EXIT_FAILURE);
             }
+
+            if (private_nh.getParam("depthlookup_out_topic", out_topic))
+            {
+                ROS_INFO(">>> Output Topic: %s", out_topic.c_str());
+            } else {
+                ROS_ERROR("Failed to get output topic parameter");
+                exit(EXIT_FAILURE);
+            }
+
+            if (private_nh.getParam("depthlookup_in_topic", in_topic))
+            {
+                ROS_INFO(">>> Input Topic: %s", in_topic.c_str());
+            } else {
+                ROS_ERROR("Failed to get input topic parameter");
+                exit(EXIT_FAILURE);
+            }
+
             message_filters::Subscriber<Image> image_sub(private_nh, depth_topic.c_str(), 1);
             message_filters::Subscriber<CameraInfo> info_sub(private_nh, depth_info.c_str(), 1);
+            people_pub = private_nh.advertise<clf_perception_vision::ExtenedPeople>(out_topic.c_str(), 1);
+
             TimeSynchronizer<Image, CameraInfo> sync(image_sub, info_sub, 10);
             sync.registerCallback(boost::bind(&DepthLookup::depth_callback, this ,_1, _2));
         }
