1639090a3e61b6f2d25e7aacafb0e9ff367a44fd
fl <fl@techfak.uni-bielefeld.de>
2018-02-08 20:24:58 +0100

    trying to fix cpu consumption

--- src/python/clf_perception_people2map.py
+++ src/python/clf_perception_people2map.py
@@ -2,6 +2,7 @@
 
 # Author: Felix Friese [ffriese AT techfak.uni-bielefeld DOT de]
 
+import time
 import rospy
 from optparse import OptionParser
 from tf import TransformListener
@@ -13,6 +14,7 @@ class ExtendedPeople2Map:
     def __init__(self, _in, _out):
         rospy.init_node('clf_perception_vision_people2map', anonymous=True)
         self.tf = TransformListener()
+        self.reference_frame = "/map"
         self.sub = rospy.Subscriber(str(_in), ExtendedPeople, self.people_cb, queue_size=2)
         self.pub = rospy.Publisher(str(_out), ExtendedPeople, queue_size=2)
         rospy.logdebug(">>> People2Map is ready.")
@@ -20,14 +22,19 @@ class ExtendedPeople2Map:
     def people_cb(self, data):
 
         frame_id = data.persons[0].pose.header.frame_id
-        if not self.tf.frameExists(frame_id) and self.tf.frameExists("/map"):
-            print('transform from %s to /map not found... discarding message' % frame_id)
+        if not self.tf.frameExists(frame_id):
+            rospy.logwarn(">>> Frame does not exist, %s" % frame_id)
+            time.sleep(0.05)
+            return
+        if not self.tf.frameExists(self.reference_frame):
+            rospy.logwarn(">>> Frame does not exist, %s" % self.reference_frame)
+            time.sleep(0.05)
             return
         try:
             for person in data.persons:
-                person.pose = self.tf.transformPose('/map', person.pose)
-
-            self.pub.publish(data)
+                self.tf.waitForTransform(self.reference_frame, person.pose, rospy.Time.now(), rospy.Duration(0.15))
+                transform_pose = self.tf.transformPose(self.reference_frame, person.pose)
+                self.pub.publish(transform_pose)
         except Exception, ex:
             rospy.logwarn(">>> Problem receiving data, %s" % str(ex))
 
