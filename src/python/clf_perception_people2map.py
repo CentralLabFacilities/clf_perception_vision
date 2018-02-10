2a500c60265632d4386ebf356322738ace7ba11f
Florian Lier <fl@techfak.uni-bielefeld.de>
2018-02-10 13:18:38 +0100

    debugging tf transform pose

--- src/python/clf_perception_people2map.py
+++ src/python/clf_perception_people2map.py
@@ -6,34 +6,33 @@ import time
 import rospy
 from optparse import OptionParser
 from tf import TransformListener
-from geometry_msgs.msg import PoseArray, Pose
+from geometry_msgs.msg import Pose, PoseStamped
+from clf_perception_vision_msgs.msg import ExtendedPeople, ExtendedPersonStamped
 
 
 class ExtendedPeople2Map:
 
     def __init__(self, _in, _out):
         rospy.init_node('clf_perception_vision_people2map', anonymous=True)
-        self.tf = TransformListener()
+        self.tf_listener = TransformListener()
         self.reference_frame = "map"
-        self.sub = rospy.Subscriber(str(_in), PoseArray, self.people_cb, queue_size=2)
-        self.pub = rospy.Publisher(str(_out), PoseArray, queue_size=2)
+        self.sub = rospy.Subscriber(str(_in), ExtendedPeople, self.people_cb, queue_size=2)
+        self.pub = rospy.Publisher(str(_out), ExtendedPeople, queue_size=2)
         rospy.loginfo(">>> People2Map is ready.")
 
     def people_cb(self, data):
-        pa = PoseArray()
-        pa.header = data.header
         try:
-            for p in data.poses:
-                # print person.pose
-                if self.tf.waitForTransform(self.reference_frame, data.header.frame_id, rospy.Time(0), rospy.Duration(1)):
-                    print "found transform"
-                    # target_frame, stamped_in, stamped_out
-                    self.tf.transformPose(self.reference_frame, p, p)
-                    pa.poses.append(p)
+            for p in data.persons:
+                print "looking up %s --> %s " % (self.reference_frame, data.header.frame_id)
+                self.tf_listener.waitForTransform(self.reference_frame, data.header.frame_id, rospy.Time.now(), rospy.Duration(1))
+                trans_pose = self.tf_listener.transformPose(self.reference_frame, p.pose)
+                print trans_pose
+                # pa.poses.append(trans_pose)
             if len(pa.poses) > 0:
-                self.pub.publish(pa)
+                # self.pub.publish(data)
+                pass
         except Exception, ex:
-            rospy.logwarn(">>> Problem receiving data, %s" % str(ex))
+            rospy.logwarn(">>> Problem looking up TF data ---> %s" % str(ex))
 
 
 if __name__ == "__main__":
